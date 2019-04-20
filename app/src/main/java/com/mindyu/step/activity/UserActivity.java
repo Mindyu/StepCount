54c2473896e12c3d41163bb23fb4a00ed8f24833
杨陈强 <yangchenqiang@bytedance.com>
2019-04-20 20:50:12 +0800

    add user_fragment
    Change-Id: Icb61c414cfa2f5076bfd6911263ec9daefd991c0

---
+++ app/src/main/java/com/mindyu/step/activity/UserActivity.java
@@ -0,0 +1,17 @@
+package com.mindyu.step.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.mindyu.step.R;
+
+public class UserActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user);
+    }
+
+    // TODO: 2019/4/20  
+}
--- app/src/main/java/com/mindyu/step/fragment/UserFragment.java
+++ app/src/main/java/com/mindyu/step/fragment/UserFragment.java
@@ -1,13 +1,16 @@
 package com.mindyu.step.fragment;
 
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
 
 import com.mindyu.step.R;
+import com.mindyu.step.activity.UserActivity;
 
 
 /**
@@ -15,17 +18,68 @@ import com.mindyu.step.R;
  */
 public class UserFragment extends Fragment {
 
+    private LinearLayout user_management;
+    private LinearLayout date_update;
+    private LinearLayout soft_update;
+    private LinearLayout sensor_test;
+    private LinearLayout about_system;
 
     public UserFragment() {
-        // Required empty public constructor
     }
 
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_user, container, false);
+        View view = inflater.inflate(R.layout.fragment_user, container, false);
+        initView(view);
+        initEvent();
+        return view;
     }
 
+    public void initView(View view) {
+        user_management = view.findViewById(R.id.user_management);
+        date_update = view.findViewById(R.id.date_update);
+        soft_update = view.findViewById(R.id.soft_update);
+        sensor_test = view.findViewById(R.id.sensor_test);
+        about_system = view.findViewById(R.id.about_system);
+    }
+
+    public void initEvent() {
+        user_management.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getContext(), UserActivity.class);
+                startActivity(intent);
+            }
+        });
+        date_update.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getContext(), UserActivity.class);
+                startActivity(intent);
+            }
+        });
+        soft_update.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getContext(), UserActivity.class);
+                startActivity(intent);
+            }
+        });
+        sensor_test.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getContext(), UserActivity.class);
+                startActivity(intent);
+            }
+        });
+        about_system.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getContext(), UserActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
 }
