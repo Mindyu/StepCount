f3194e1e0981c4a851292098405be13c9772525d
杨陈强 <yangchenqiang@bytedance.com>
2019-04-20 18:54:13 +0800

    add bottom bar
    Change-Id: I8ee7ab3396ecf7fb1e365693bbcc5a44eeabc505

--- app/src/main/java/com/mindyu/step/activity/MainActivity.java
+++ app/src/main/java/com/mindyu/step/activity/MainActivity.java
@@ -1,133 +1,60 @@
 package com.mindyu.step.activity;
 
-import android.content.ComponentName;
-import android.content.Context;
-import android.content.Intent;
-import android.content.ServiceConnection;
 import android.os.Bundle;
-import android.os.IBinder;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.widget.TextView;
+import android.view.Window;
+import android.view.WindowManager;
 
+import com.hjm.bottomtabbar.BottomTabBar;
 import com.mindyu.step.R;
-import com.mindyu.step.step.UpdateUiCallBack;
-import com.mindyu.step.step.service.StepService;
-import com.mindyu.step.step.utils.SharedPreferencesUtils;
-import com.mindyu.step.view.StepArcView;
+import com.mindyu.step.fragment.UserFragment;
+import com.mindyu.step.fragment.StepFragment;
+import com.mindyu.step.fragment.HotTopicFragment;
+import com.mindyu.step.fragment.HistoryFragment;
 
 /**
  * 记步主页
  */
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
-    private TextView tv_data;
-    private StepArcView cc;
-    private TextView tv_set;
-    private TextView tv_isSupport;
-    private SharedPreferencesUtils sp;
+public class MainActivity extends AppCompatActivity {
 
-    private void assignViews() {
-        tv_data = (TextView) findViewById(R.id.tv_data);
-        cc = (StepArcView) findViewById(R.id.cc);
-        tv_set = (TextView) findViewById(R.id.tv_set);
-        tv_isSupport = (TextView) findViewById(R.id.tv_isSupport);
-    }
+    private BottomTabBar mBottomBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);//不显示程序的标题栏
+//        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);//去掉信息栏
         setContentView(R.layout.activity_main);
-        assignViews();
-        initData();
-        addListener();
+        mBottomBar = findViewById(R.id.bottom_bar);
+
+        mBottomBar.init(getSupportFragmentManager(), 1080, 1920)
+//                .setImgSize(50, 50)
+//                .setFontSize(28)
+//                .setTabPadding(10, 6, 4)
+//                .setChangeColor(Color.parseColor("#2784E7"),Color.parseColor("#282828"))
+                .addTabItem("首页", R.mipmap.ic_common_tab_index_select, R.mipmap.ic_common_tab_index_unselect, StepFragment.class)
+                .addTabItem("历史", R.mipmap.ic_common_tab_publish_select, R.mipmap.ic_common_tab_publish_unselect, HistoryFragment.class)
+                .addTabItem("发现", R.mipmap.ic_common_tab_hot_select, R.mipmap.ic_common_tab_hot_unselect, HotTopicFragment.class)
+                .addTabItem("我的", R.mipmap.ic_common_tab_user_select, R.mipmap.ic_common_tab_user_unselect, UserFragment.class)
+//                .isShowDivider(true)
+//                .setDividerColor(Color.parseColor("#373737"))
+//                .setTabBarBackgroundColor(Color.parseColor("#FFFFFF"))
+                .setOnTabChangeListener(new BottomTabBar.OnTabChangeListener() {
+                    @Override
+                    public void onTabChange(int position, String name, View view) {
+                        mBottomBar.setSpot(position, false);
+                    }
+                })
+                .setSpot(2, true);
     }
 
-
-    private void addListener() {
-        tv_set.setOnClickListener(this);
-        tv_data.setOnClickListener(this);
-    }
-
-    private void initData() {
-        sp = new SharedPreferencesUtils(this);
-        //获取用户设置的计划锻炼步数，没有设置过的话默认7000
-        String planWalk_QTY = (String) sp.getParam("planWalk_QTY", "7000");
-        //设置当前步数为0
-        cc.setCurrentCount(Integer.parseInt(planWalk_QTY), 0);
-        tv_isSupport.setText("计步中...");
-        setupService();
-    }
-
-
-    private boolean isBind = false;
-
-    /**
-     * 开启计步服务
-     */
-    private void setupService() {
-        Intent intent = new Intent(this, StepService.class);
-        isBind = bindService(intent, conn, Context.BIND_AUTO_CREATE);
-        startService(intent);
-    }
-
-    /**
-     * 用于查询应用服务（application Service）的状态的一种interface，
-     * 更详细的信息可以参考Service 和 context.bindService()中的描述，
-     * 和许多来自系统的回调方式一样，ServiceConnection的方法都是进程的主线程中调用的。
-     */
-    ServiceConnection conn = new ServiceConnection() {
-        /**
-         * 在建立起于Service的连接时会调用该方法，目前Android是通过IBind机制实现与服务的连接。
-         * @param name 实际所连接到的Service组件名称
-         * @param service 服务的通信信道的IBind，可以通过Service访问对应服务
-         */
-        @Override
-        public void onServiceConnected(ComponentName name, IBinder service) {
-            StepService stepService = ((StepService.StepBinder) service).getService();
-            //设置初始化数据
-            String planWalk_QTY = (String) sp.getParam("planWalk_QTY", "7000");
-            cc.setCurrentCount(Integer.parseInt(planWalk_QTY), stepService.getStepCount());
-
-            //设置步数监听回调
-            stepService.registerCallback(new UpdateUiCallBack() {
-                @Override
-                public void updateUi(int stepCount) {
-                    String planWalk_QTY = (String) sp.getParam("planWalk_QTY", "7000");
-                    cc.setCurrentCount(Integer.parseInt(planWalk_QTY), stepCount);
-                }
-            });
-        }
-
-        /**
-         * 当与Service之间的连接丢失的时候会调用该方法，
-         * 这种情况经常发生在Service所在的进程崩溃或者被Kill的时候调用，
-         * 此方法不会移除与Service的连接，当服务重新启动的时候仍然会调用 onServiceConnected()。
-         * @param name 丢失连接的组件名称
-         */
-        @Override
-        public void onServiceDisconnected(ComponentName name) {
-
-        }
-    };
-
-
-    @Override
-    public void onClick(View v) {
-        switch (v.getId()) {
-            case R.id.tv_set:
-                startActivity(new Intent(this, SetPlanActivity.class));
-                break;
-            case R.id.tv_data:
-                startActivity(new Intent(this, HistoryActivity.class));
-                break;
+    public void setShowTabBar(boolean isShow){
+        if (isShow){
+            mBottomBar.getTabBar().setVisibility(View.VISIBLE);
+        }else {
+            mBottomBar.getTabBar().setVisibility(View.GONE);
         }
     }
 
-    @Override
-    public void onDestroy() {
-        super.onDestroy();
-        if (isBind) {
-            this.unbindService(conn);
-        }
-    }
 }
--- app/src/main/java/com/mindyu/step/fragment/HistoryFragment.java
+++ app/src/main/java/com/mindyu/step/fragment/HistoryFragment.java
@@ -1,10 +1,11 @@
-package com.mindyu.step.activity;
+package com.mindyu.step.fragment;
+
 
 import android.os.Bundle;
-import android.support.annotation.Nullable;
-import android.support.v7.app.AppCompatActivity;
+import android.support.v4.app.Fragment;
 import android.util.TypedValue;
 import android.view.Gravity;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
@@ -12,55 +13,50 @@ import android.widget.LinearLayout;
 import android.widget.ListView;
 import android.widget.TextView;
 
-import com.orhanobut.logger.Logger;
-
-import java.util.List;
-
 import com.mindyu.step.R;
 import com.mindyu.step.adapter.CommonAdapter;
 import com.mindyu.step.adapter.CommonViewHolder;
 import com.mindyu.step.step.bean.StepData;
 import com.mindyu.step.step.utils.DbUtils;
+import com.orhanobut.logger.Logger;
+
+import java.util.List;
+
 
 /**
- * Created by yuandl on 2016-10-18.
+ * A simple {@link Fragment} subclass.
  */
+public class HistoryFragment extends Fragment {
 
-public class HistoryActivity extends AppCompatActivity {
     private LinearLayout layout_titlebar;
-    private ImageView iv_left;
-    private ImageView iv_right;
     private ListView lv;
 
-    private void assignViews() {
-        layout_titlebar = (LinearLayout) findViewById(R.id.layout_titlebar);
-        iv_left = (ImageView) findViewById(R.id.iv_left);
-        iv_right = (ImageView) findViewById(R.id.iv_right);
-        lv = (ListView) findViewById(R.id.lv);
+    private void assignViews(View view) {
+        layout_titlebar = view.findViewById(R.id.layout_titlebar);
+        lv = view.findViewById(R.id.lv);
+    }
+
+    public HistoryFragment() {
+
     }
 
     @Override
-    protected void onCreate(@Nullable Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.ac_history);
-        assignViews();
-        iv_left.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-               finish();
-            }
-        });
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_history, container, false);
+        assignViews(view);
         initData();
+        return view;
     }
 
     private void initData() {
         setEmptyView(lv);
         if(DbUtils.getLiteOrm()==null){
-            DbUtils.createDb(this, "jingzhi");
+            DbUtils.createDb(this.getContext(), "jingzhi");
         }
         List<StepData> stepDatas =DbUtils.getQueryAll(StepData.class);
         Logger.d("stepDatas="+stepDatas);
-        lv.setAdapter(new CommonAdapter<StepData>(this,stepDatas,R.layout.item) {
+        lv.setAdapter(new CommonAdapter<StepData>(this.getContext(),stepDatas,R.layout.item) {
             @Override
             protected void convertView(View item, StepData stepData) {
                 TextView tv_date= CommonViewHolder.get(item,R.id.tv_date);
@@ -72,7 +68,7 @@ public class HistoryActivity extends AppCompatActivity {
     }
 
     protected <T extends View> T setEmptyView(ListView listView) {
-        TextView emptyView = new TextView(this);
+        TextView emptyView = new TextView(this.getContext());
         emptyView.setLayoutParams(new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT));
         emptyView.setText("暂无数据！");
         emptyView.setGravity(Gravity.CENTER);
